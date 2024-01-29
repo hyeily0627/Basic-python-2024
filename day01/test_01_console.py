b3a7b437220a72183149942a51d801bed4c08b89
hyeily0627 <hj_o5h@naver.com>
2024-01-29 15:24:59 +0900

    1일차 업데이트

---
+++ day01/test_01_console.py
@@ -0,0 +1,20 @@
+# date : 2024 - 01 - 29
+# desc : 변수 설명
+
+# 변수(Variable) - 변하는 수. 종류나 크기에 제약이 없음 
+var01 = 10
+print(var01)
+
+var01 = False
+print(var01)
+
+var01 = 'Love is givingS2'
+print(var01)
+
+# 단, 변수명을 지정시 아래 사항이 있으면 사용불가
+## 1 숫자로 시작 ex) 01var
+## 2 공백 ex) var iavle
+## 3 언더바(_)이외의 특수문자 ex) var@# 
+var_01 = 0 # 가능
+_var_01 = 1 # 가능 / 다만, 되도록 _로 시작하는 변수는 사용하지 말 것
+
---
+++ day01/test_02_console.py
@@ -0,0 +1,8 @@
+# date : 2024 - 01 - 29
+# desc : 콘솔 출력 
+
+# 콘솔출력을 위해서는 print() 함수를 사용(#주석은 소스 실행에 영향)
+print('hello, world')
+
+# python test01_console.py 로 콘솔실행
+# VS Code에서 ctrl + F5 (실행 > 디버깅 없이 실행)
